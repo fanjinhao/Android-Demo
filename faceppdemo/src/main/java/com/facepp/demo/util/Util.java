9a30f932a43634758c7c2b687e3bdb470b5b1dbe
fanjinhao <fanjinhao@wewillpro.com>
2019-02-13 12:21:27 +0800

    Initial commit

---
+++ faceppdemo/src/main/java/com/facepp/demo/util/Util.java
@@ -0,0 +1,11 @@
+package com.facepp.demo.util;
+
+public class Util {
+
+	//在这边填写 API_KEY 和 API_SECRET
+	public static String API_KEY = "qdnu0z0qfPp995WlX-nPtIAVGcNHeAiM";
+	public static String API_SECRET = "Fp22yq2OMg59Fb3CsGH_3LwRswKWN4Ke";
+
+	public static String CN_LICENSE_URL = "https://api-cn.faceplusplus.com/sdk/v3/auth";
+	public static String US_LICENSE_URL = "https://api-us.faceplusplus.com/sdk/v3/auth";
+}
